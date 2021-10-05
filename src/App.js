4a4018750ca4fbc13472babd836959a6ce0cf6e6
mark novelero <mark.novelero@gmail.com>
2021-10-05 18:10:35 -0400

    slideshow complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Fade as SlideShowFade } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const fadeImages = [
+  "https://i.imgur.com/jTeMXIX.png",
+ "https://i.imgur.com/FCV9ZlD.png",
+ "https://i.imgur.com/gX72rNA.png",
+ "https://i.imgur.com/oBADh8G.png",
+ "https://i.imgur.com/hbHsgPO.png"
+];
 
-export default App;
+const caption = [
+  "From the South East Asia to New York City", 
+  "Professional Achievements", 
+  "Professional Achievements", 
+  "Current Project: M.O.R.U.S Zine Collection Website", 
+  "Career Trajectory and Future Goals", 
+]
+
+
+
+class App extends Component {
+
+ state = {}
+
+ render() {
+  return(
+   <div className = "slide-div">
+       <SlideShowFade duration = {25000}>
+      <div className="each-fade">
+        <div className="caption-title">
+        <h2 className = "caption">{caption[0]}</h2>
+        </div>
+        <img src={fadeImages[0]} className = "vh-image"/>
+      </div>
+      <div className="each-fade">
+        <div className="prof-title">
+          <h2 className = "professional">{caption[1]}</h2>
+        </div>
+        <img src={fadeImages[1]} className = "vh-image"/>
+      </div>
+      <div className="each-fade">
+      <div className="prof-title">
+        <h2 className = "professional">{caption[1]}</h2>
+      </div> 
+        <img src={fadeImages[2]} className = "vh-image"/>
+      </div>
+      <div className="each-fade">
+        <div className = "zine-title">
+        <h2 className = "zine">{caption[3]}</h2>
+        </div>
+        <img src={fadeImages[3]} className = "vh-image"/>
+      </div>
+      <div className="each-fade">
+        <div className = "career-title">
+          <h2 className = "career">{caption[4]}</h2>
+        </div>
+        <img src={fadeImages[4]} className = "vh-image"/>
+      </div>
+    </SlideShowFade>
+   </div>
+    )
+   }
+ }
+
+
+export default App
